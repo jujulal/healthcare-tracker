7f931a26bb5895b67df4af1feafc3c9a58d54581
Bibek Shrestha <bibekshrestha@bibeks-mbp.home>
2020-09-03 10:01:05 -0400

    initize the project

---
+++ healthcare-tracker/src/main/java/com/centene/healthcaretracker/HealthcareTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.centene.healthcaretracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcareTrackerApplication.class, args);
+	}
+
+}
---
+++ healthcare-tracker/src/test/java/com/centene/healthcaretracker/HealthcareTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centene.healthcaretracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
